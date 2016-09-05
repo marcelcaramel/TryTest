34fbf1d12f8bf2543f4acdd5892789bb7a81c144
Simon Laforge <simonlaforgechalifour@gmail.com>
2016-09-05 18:19:29 -0400

    Add Project
    Added Project

---
+++ Tryharding/Assets/Scripts/Trying_Super_Hard_Right_Na.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trying_Super_Hard_Right_Na : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
