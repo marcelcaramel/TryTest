0a497b4fddc8321c5c659a7e8637d0085f496c6f
Simon Laforge <simonlaforgechalifour@gmail.com>
2016-09-11 18:50:59 -0400

    Added Plenty o' stuff
    title

---
+++ TheFarm/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    private GameObject theCamera;
+    private GameObject camColLeft;
+    private GameObject camColRight;
+
+    // Use this for initialization
+    void Start () {
+        theCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        camColLeft = GameObject.FindGameObjectWithTag("camColLeft");
+        camColRight = GameObject.FindGameObjectWithTag("camColRight");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnTriggerStay2D (Collider2D theCol)
+    {
+        if (theCol.gameObject.tag == "Player")
+        {
+            if (this.tag == "camColLeft")
+            {
+                theCamera.transform.Translate(new Vector3(-0.035f, 0, 0));
+            }
+            if (this.tag == "camColRight")
+            {
+                theCamera.transform.Translate(new Vector3(0.035f, 0, 0));
+            }
+        }
+    }
+}
---
+++ TheFarm/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float horizMovement;
+    float vertMovement;
+
+    Animator theAnimator;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        
+    }
+
+    void Movement()
+    {
+        CheckInputs();
+
+        horizMovement = Input.GetAxis("Horizontal");
+        vertMovement = Input.GetAxis("Vertical") ;
+
+        
+
+        this.transform.Translate(new Vector3(horizMovement / 100, 0, 0));
+        this.transform.Translate(new Vector3(0, vertMovement / 50, 0));
+
+
+    }
+
+    void CheckInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("pressed space");
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 100));
+        }
+    }
+
+    void AnimateMovement(float horizontalValue, float verticalValue)
+    {
+        
+    }
+}
